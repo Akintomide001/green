dadd10fad23007bd4660a71f83edbdbe57ad5ea1
tommy <akintomide001@gmail.com>
2023-06-29 11:40:28 +0100

    dashboard

--- pages/dropdown.js
+++ pages/dropdown.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { FaTimes } from "react-icons/fa"
 import { GiHamburgerMenu } from "react-icons/gi"
-import { useEffect, useRef,} from "react";
+import { useEffect, useRef, } from "react";
 
 export default function Dropdown() {
     const showNavMenu = useRef();
@@ -9,40 +9,42 @@ export default function Dropdown() {
     const navMenu = useRef();
     const [currentlyShowing, setCurrentlyShowing] = useState(false)
 
-    useEffect(()=>{
-        if (!currentlyShowing){
+    useEffect(() => {
+        if (!currentlyShowing) {
             hideNavMenu.current.style.display = "none"
             showNavMenu.current.style.display = "block"
-        }else{
+        } else {
             hideNavMenu.current.style.display = "block"
             showNavMenu.current.style.display = "none"
         }
-    },[currentlyShowing])
+    }, [currentlyShowing])
     return (
 
         <>
             <div className="nav-menu" ref={navMenu}>
-                        <ul>
-                            <li><a href="">Home</a></li>
-                            <li><a href="">About Us</a></li>
-                            <li><a href="">Services</a></li>
-                            <li><a href="">Products/produce</a></li>
-                            <li><a href="">Gallery</a></li>
-                            <li><a href="">Blog</a></li>
-                            <button className="btn"><a href="">Contact Us</a></button>
-                        </ul>
-                    </div>
-                    <div>
-                    <div ref={showNavMenu} onClick={()=>{
-                        navMenu.current.style.display = "flex";
-                        setCurrentlyShowing(!currentlyShowing)}} className="open-menu"><GiHamburgerMenu className="drop" /></div>
-                    <div ref={hideNavMenu} onClick={()=>{
-                        navMenu.current.style.display = "none";
-                        setCurrentlyShowing(!currentlyShowing)}} className="close-menu"><FaTimes className="drop" /></div>
-                    
-                    {/*mobile menu*/}
-                    {/*mobile menu end*/}
-                </div>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Products/produce</a></li>
+                    <li><a href="">Gallery</a></li>
+                    <li><a href="">Blog</a></li>
+                    <button className="btn"><a href="">Contact Us</a></button>
+                </ul>
+            </div>
+            <div>
+                <div ref={showNavMenu} onClick={() => {
+                    navMenu.current.style.display = "flex";
+                    setCurrentlyShowing(!currentlyShowing)
+                }} className="open-menu"><GiHamburgerMenu className="drop" /></div>
+                <div ref={hideNavMenu} onClick={() => {
+                    navMenu.current.style.display = "none";
+                    setCurrentlyShowing(!currentlyShowing)
+                }} className="close-menu"><FaTimes className="drop" /></div>
+
+                {/*mobile menu*/}
+                {/*mobile menu end*/}
+            </div>
         </>
     )
 }
